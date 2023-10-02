8be94becadf30e62f4bdb8d40413da5a75a894bc
Ghosme27 <113557448+Ghosme27@users.noreply.github.com>
2023-10-02 20:26:11 +0200

    .

---
+++ ejercio 5.py
@@ -0,0 +1,4 @@
+horas_trabajadas=input('¿cuantas horas trabajas?')
+pago_por_hora=input('¿cuanto cobras por hora?')
+cobro_al_dia=(horas_trabajadas * pago_por_hora)
+print('por',horas_trabajadas,'cobraras',cobro_al_dia)
